7776b9b3ef36d9137254f45a8db0a25c6b386e3c
krishan kumar <krishan kumar>
2020-06-16 11:02:28 +0530

    lever 2 security--HASHING

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+// const encrypt = require('mongoose-encryption');
 
 const app = express();
 app.use(express.json());
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema({
 
 // const secret = "thisisalittlesecret";
 //if you want to add encryption on any other fileds then add those field to array ['passwrod','email'] 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -41,7 +42,7 @@ app.post('/register', function(req, res) {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -56,8 +57,10 @@ app.post('/register', function(req, res) {
 app.post('/login', function(req, res) {
     User.findOne({ email: req.body.username }, function(err, foundUser) {
         if (foundUser) {
-            if (foundUser.password === req.body.password) {
+            if (foundUser.password === md5(req.body.password)) {
                 res.render('secrets');
+            } else {
+                res.send("wrong password !!! please try again")
             }
 
         } else {
